ad31d9d345a963bf74f4211029a778d5575b4493
Nayan Parvez <nayanparvez90@gmail.com>
2025-04-09 13:02:34 -0700

    scripts added and buildspec appspec updated

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "running restart.sh ..." >> /home/ubuntu/restart.log
+# echo "running restart.sh ..." >> /home/ubuntu/restart.log
 
-bash /home/ubuntu/scripts/stop.sh
-sleep 2  # Give time to shut down properly
-bash /home/ubuntu/scripts/start.sh
+# bash /home/ubuntu/scripts/stop.sh
+# sleep 2  # Give time to shut down properly
+# bash /home/ubuntu/scripts/start.sh
 
-if [ $? -eq 0 ]; then
-    echo "restart.sh completed successfully" >> /home/ubuntu/restart.log
-else
-    echo "restart.sh failed!" >> /home/ubuntu/restart.log
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#     echo "restart.sh completed successfully" >> /home/ubuntu/restart.log
+# else
+#     echo "restart.sh failed!" >> /home/ubuntu/restart.log
+#     exit 1
+# fi
