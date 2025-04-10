ca4ff8c6ecdcf42c6198fe9ee808c3801ecd9044
Nayan Parvez <nayanparvez90@gmail.com>
2025-04-09 18:56:29 -0700

    app file updated

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -22,5 +22,5 @@ def health():
     return 'OK', 200
 
 if __name__ == '__main__':
-    debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
-    app.run(debug=debug_mode, host="0.0.0.0", port=5000)
+    # debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
+    app.run(debug=True, host="0.0.0.0", port=5000)
